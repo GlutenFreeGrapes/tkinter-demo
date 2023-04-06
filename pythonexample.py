44a594b89594afbd075ccdc4bb18407a80d57abe
GlutenFreeGrapes <daniellian238@gmail.com>
2023-04-06 13:17:31 -0400

    Create pythonexample.py

---
+++ pythonexample.py
@@ -0,0 +1,20 @@
+def function():                     #makes a new function
+    print('This is a function')     #prints "This is a function"
+
+i=True                              #i is True
+print(i)                            #prints True
+print(type(i))                      #prints 'bool'
+
+i=0                                 #i is 0
+print(i)                            #prints 0
+print(type(i))                      #prints 'int'
+
+i=0.1                               #i is 0.1
+print(i)                            #prints 0.1
+print(type(i))                      #prints 'float'
+
+i="abc"                             #i is "abc"
+print(i)                            #prints abc
+print(type(i))                      #prints 'str'
+
+function()                          #does whatever is in the function
